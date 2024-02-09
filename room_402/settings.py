867345000b66cb39ac39a013f9fb236fa1ecae12
bfy <you@example.com>
2024-02-09 18:31:17 +0800

    fix time_zone

--- room_402/settings.py
+++ room_402/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest framework',
     'room_app.apps.RoomAppConfig',
 ]
 
@@ -105,11 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
