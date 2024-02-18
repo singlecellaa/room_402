d3f996f688b5cd9550f44d0708c442c881b1bf80
BFY <1603724036@qq.com>
2024-02-18 15:03:10 +0800

    fix: warnings of objects

--- message/models.py
+++ message/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Notice(models.Model):
+    objects = models.Manager()
     source=models.IntegerField(choices=((1,'系统消息'),(2,'社团管理员通知'),(3,'房间管理员通知')))
     time = models.DateField(auto_now_add=True)  # 反馈创建时间
     content= models.TextField()
     read=models.BooleanField(blank=False)       # 判断是否已读
 
 class broadcast(models.Model):
+    objects = models.Manager()
     broadcast = models.TextField()
+
 class Dsyfunc(models.Model):
+    objects=models.Manager()
     item = models.CharField(max_length=15)
     description = models.TextField()
     img = models.ImageField()
@@ -17,5 +23,6 @@ class Dsyfunc(models.Model):
 
 
 class Feedback(models.Model):
+    objects = models.Manager()
     time = models.DateField(auto_now_add=True)  # 反馈创建时间
     description = models.TextField()  # 反馈内容
--- message/views.py
+++ message/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from message.serializer import DsyfuncSerializer,FeedbackSerializer,NoticeSerializer
+from message.serializer import DsyfuncSerializer, FeedbackSerializer, NoticeSerializer
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from message import models
@@ -16,7 +16,7 @@ class DsyfuncView(ModelViewSet):
         if not dsyfunc.is_valid():
             return Response(dsyfunc.errors)
         else:
-            dsyfunc_instance=dsyfunc.save()
+            dsyfunc_instance = dsyfunc.save()
             return Response(dsyfunc.data)
 
 
@@ -24,33 +24,41 @@ class FeedbackView(ModelViewSet):
     queryset = models.Feedback.objects
     serializer_class = FeedbackSerializer
 
-    def save_feedback(self,request):
-        feedback=FeedbackSerializer(data=request.data)
+    def save_feedback(self, request):
+        feedback = FeedbackSerializer(data=request.data)
 
         if not feedback.is_valid():
             return Response(feedback.errors)
         else:
-            feedback_instance=feedback.save()
+            feedback_instance = feedback.save()
 
             return Response(feedback.data)
+
+
+class BroadcastView(ModelViewSet):
+    pass
+
 
 class NoticeView(ModelViewSet):
     queryset = models.Notice.objects.all()
     serializer_class = NoticeSerializer
+
     @action(methods=["post"], detail=True)
-    def read(self, request, pk=None):   #标记已读
+    def read(self, request, pk=None):  # 标记已读
         notice: models.Notice = self.get_object()
         notice.read = True
         notice.save()
         serializer = self.get_serializer(notice)
         return Response(serializer.data)
 
+
 class StudentNoticeView(APIView):
-    #default: all
-    #filter: all, not read, read
-    pass 
+    # default: all
+    # filter: all, not read, read
+    pass
+
 
 class ManagerNoticeView(APIView):
-    #below without “首页”, the same to StudentNoticeView perhaps
-    #publish notification
-    pass 
+    # below without “首页”, the same to StudentNoticeView perhaps
+    # publish notification
+    pass
