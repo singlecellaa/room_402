9f560c398fc8d7f1083f9723c7b8fe5a4f57f0c7
chen <singlecella@outlook.com>
2024-02-26 10:12:52 +0800

    ReservationSerializer small modifying

--- reservation/views.py
+++ reservation/views.py
@@ -80,7 +80,6 @@ class ReservationSerializer(HookSerializer,serializers.ModelSerializer):
         fields = ['id','start_time','end_time','user','state']
         extra_kwargs = {
             'state':{'read_only':True,'source':'get_state_display'},
-            'user':{'read_only':True}
         }
         
     def nb_user(self,obj):
@@ -89,29 +88,29 @@ class ReservationSerializer(HookSerializer,serializers.ModelSerializer):
         ser = UserModelSerializer(instance=queryset,many=True)
         return ser.data
     
-    def validate_start_time(self,value):
-        now = datetime.datetime.now().astimezone()
-        if value > now:
-            return value
-        else:
-            raise exceptions.ValidationError('起始时间应不早于当前时间')
+    # def validate_start_time(self,value):
+    #     now = datetime.datetime.now().astimezone()
+    #     if value > now:
+    #         return value
+    #     else:
+    #         raise exceptions.ValidationError('起始时间应不早于当前时间')
         
-    def validate(self,attrs):
-        start_time = attrs['start_time']
-        end_time = attrs['end_time']
-        if start_time >= end_time:
-            raise exceptions.ValidationError('结束时间应晚于起始时间')
-        if start_time - end_time > datetime.timedelta(hours=3):
-            raise exceptions.ValidationError('预约的时间过长')
-        queryset = models.Reservation.objects.all()
-        ser = ReservationTimeSerializer(instance=queryset,many=True)
-        data = ser.data
-        for i in data:
-            s_time = datetime.datetime.fromisoformat(i['start_time'])
-            e_time = datetime.datetime.fromisoformat(i['end_time'])
-            if s_time < start_time < e_time or s_time < end_time < e_time or start_time < s_time < end_time or start_time < e_time < end_time or (s_time==start_time and e_time==end_time):
-                raise exceptions.ValidationError('与已预定的时间冲突')
-        return attrs
+    # def validate(self,attrs):
+    #     start_time = attrs['start_time']
+    #     end_time = attrs['end_time']
+    #     if start_time >= end_time:
+    #         raise exceptions.ValidationError('结束时间应晚于起始时间')
+    #     if start_time - end_time > datetime.timedelta(hours=3):
+    #         raise exceptions.ValidationError('预约的时间过长')
+    #     queryset = models.Reservation.objects.all()
+    #     ser = ReservationTimeSerializer(instance=queryset,many=True)
+    #     data = ser.data
+    #     for i in data:
+    #         s_time = datetime.datetime.fromisoformat(i['start_time'])
+    #         e_time = datetime.datetime.fromisoformat(i['end_time'])
+    #         if s_time < start_time < e_time or s_time < end_time < e_time or start_time < s_time < end_time or start_time < e_time < end_time or (s_time==start_time and e_time==end_time):
+    #             raise exceptions.ValidationError('与已预定的时间冲突')
+    #     return attrs
     
 class ReservationView(ModelViewSet):
     authentication_classes = []
