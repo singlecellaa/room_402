15643702940cdf2bfa44fb1240d141e6c1f587ad
chen <singlecella@outlook.com>
2024-03-21 15:25:18 +0800

    fix the breach evaluation

--- ext/evaluate.py
+++ ext/evaluate.py
@@ -23,7 +23,7 @@ def evaluate_breach():
         start_ddl = reservation.start_time + datetime.timedelta(minutes=30)
         end_ddl = reservation.end_time + datetime.timedelta(minutes=10)
 
-        if now > start_ddl and reservation.on_time_isnull:
+        if now > start_ddl and not reservation.on_time:
             if reservation.sign_in_time != None and reservation.sign_in_time < start_ddl:
                 reservation.on_time = 1
             else:
@@ -31,9 +31,10 @@ def evaluate_breach():
                 #写入notice
                 notice =Notice.objects.create(content="您预约使用{}--{} 402房间未准时到达，记录违约".format(reservation.start_time,reservation.end_time),
                                           source=1)
-                notice.shared_people.set(reservation.user)
+                user = User.objects.filter(id=reservation.user.id)
+                notice.shared_people.set(user)
 
-        if now > end_ddl and reservation.over_time_isnull:
+        if now > end_ddl and not reservation.over_time:
             if reservation.sign_out_time != None and reservation.sign_out_time < end_ddl:
                 reservation.over_time = 1
             else:
@@ -43,7 +44,8 @@ def evaluate_breach():
                     content="您预约使用{}--{} 402房间未准时离开，记录违约".format(reservation.start_time,
                                                                                  reservation.end_time),
                     source=1)
-                notice.shared_people.set(reservation.user)
+                user = User.objects.filter(id=reservation.user.id)
+                notice.shared_people.set(user)
         reservation.save()
         
 def get_breach_time():
